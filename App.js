7f4e7d52b3c8763a777ca84175074c7b51336c2c
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-23 11:34:48 +0100

    Learn Flatlist

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
-import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, TouchableOpacity } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("Shaun");
-  const [person, setPerson] = useState({name: 'mario', age: 40});
-  const changeName = () => {
-    setName('chun-li')
-    setPerson({name: 'kene', age: 29})
-  }
-  return ( 
+
+  return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello World</Text>
-      </View>
-      <View style={styles.body}>
-        <Text>My name is {name}</Text>
-        <Text>New name is {person.name}</Text>
-        <Text>I am {person.age} year old</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button title="update NAme" onPress={changeName} />
-      </View>
-      <StatusBar style="auto" />
+      <FlatList 
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({item}) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.listItem}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      {/* <ScrollView>
+              { people.map(item => (
+        <View key={item.key}>
+          <Text style={styles.listItem}>{item.name}</Text>
+        </View>
+    ))}
+      </ScrollView> */}
     </View>
   );
 }
@@ -30,19 +30,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20
+  input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "#777",
+    padding: 30,
+    margin: 10,
+    width: 200,
   },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20,
+  listItem: {
+    backgroundColor: '#657',
+    width: 150,
+    height: 150,
+    margin: 5,
+    fontSize: 24,
+    padding: 30,
   }
 });

bd5615e8b66ad23c7485cee56392cb42899e7d61
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-23 01:21:07 +0100

    use State to modify texts

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  return (
+  const [name, setName] = useState("Shaun");
+  const [person, setPerson] = useState({name: 'mario', age: 40});
+  const changeName = () => {
+    setName('chun-li')
+    setPerson({name: 'kene', age: 29})
+  }
+  return ( 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>My name is {name}</Text>
+        <Text>New name is {person.name}</Text>
+        <Text>I am {person.age} year old</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="update NAme" onPress={changeName} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +34,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  }
 });
